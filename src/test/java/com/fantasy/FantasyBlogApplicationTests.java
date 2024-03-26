4318dd118a68e1c2371bc171f2e4dc0258004e12
huangfan <huangfan@kedacom.com>
2024-03-26 10:53:14 +0800

    代码生成器

--- src/test/java/com/fantasy/FantasyBlogApplicationTests.java
+++ src/test/java/com/fantasy/FantasyBlogApplicationTests.java
@@ -1,8 +1,8 @@
 package com.fantasy;
 
-//import com.baomidou.mybatisplus.generator.FastAutoGenerator;
-//import com.baomidou.mybatisplus.generator.config.OutputFile;
-//import com.baomidou.mybatisplus.generator.config.po.LikeTable;
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.po.LikeTable;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,26 +11,26 @@ import java.util.Collections;
 @SpringBootTest
 class FantasyBlogApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
-    /*
-    @Test
+
+//    @Test
     void createProject() {
-        FastAutoGenerator.create("jdbc:mysql://127.0.0.1:3306/nblog",
-                        "root", "1234")
+        FastAutoGenerator.create("jdbc:mysql://110.40.221.157:3306/Feeding?useUnicode=true&zeroDateTimeBehavior=convertToNull&characterEncoding=utf-8&useSSL=false",
+                        "root", "mhs-mysql4001051166")
                 .globalConfig(builder -> {
                     builder.author("Fantasy0521") // 设置作者
                             //swagger 用于生成html格式的api文档
-//                            .enableSwagger() // 开启 swagger 模式
+                            .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
-                            .outputDir("D:\\代码生成器\\Fantasy-Blog"); // 指定输出目录
+                            .outputDir("D:\\代码生成器\\Fantasy-CommonSpringBoot"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("com") // 设置父包名
                             .moduleName("fantasy") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\代码生成器\\Fantasy-Blog")); // 设置mapperXml生成路径
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\代码生成器\\Fantasy-CommonSpringBoot")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
 //                    builder.addInclude("t_simple") // 设置需要生成的表名
@@ -41,7 +41,7 @@ class FantasyBlogApplicationTests {
                 //设置生成代码使用额 模板技术
 //                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
                 .execute();
-    }*/
+    }
 
 
 }
